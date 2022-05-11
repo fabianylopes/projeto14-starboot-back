c0c4e09487d32e1dcf16a8ca94122782b908e519
= <f7lipe@me.com>
2022-05-10 23:42:53 -0300

    feat: insert suggestion

--- src/app.js
+++ src/app.js
@@ -26,8 +26,8 @@ app.post('/coffees', async (req, res) =>{
 
 app.get('/coffees', async(req, res)=>{
     /*
-    [] Buscar produtos no banco de dados 
-    [] Enviar resposta da requisição 
+    [x] Buscar produtos no banco de dados 
+    [x] Enviar resposta da requisição 
     */
 
     try {
@@ -39,6 +39,30 @@ app.get('/coffees', async(req, res)=>{
     }
 })
 
+
+app.post('/suggestions', async(req, res)=>{
+    /*
+    [] Validar body da requisição 
+    [] Inserir produto no banco de dados 
+    [] Enviar resposta da requisição 
+    */
+
+    const suggestion = req.body
+    
+    try {
+        const suggestionCollection = await db.collection("suggestions")
+        suggestionCollection.insertOne(
+            {
+                description: suggestion.description, 
+                product_id: suggestion.product_id, 
+                date: Date.now()
+            }
+        )
+        res.sendStatus(201)    
+    } catch (error) {
+        console.log("Deu ruim pra inserir a sugestão no banco", error)
+    }
+})
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(('Running on ' + port));
