9b4693aa193611b2dd4b86b575b40a9fbb273e77
= <f7lipe@me.com>
2022-05-10 23:05:48 -0300

    feat bonus: insert product

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import express, { json } from "express";
 import cors from "cors";
+import db from "./db.js";
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-app.listen(process.env.PORT, () => {
-    console.log(('Running on http://localhost:'));
+app.post('/coffees', async (req, res) =>{
+    const coffee = req.body
+    try {
+        const coffeeCollection = await db.collection("products")
+        coffeeCollection.insertOne(coffee)
+        res.sendStatus(200)
+    } catch (error) {
+        console.log("Deu ruim pra inserir o café no banco", error)
+    }
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(('Running on ' + port));
 });
+
--- src/db.js
+++ src/db.js
@@ -1,9 +1,19 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import chalk from 'chalk'
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-await mongoClient.connect();
-const db = mongoClient.db('starboot');
+let db = null; 
+
+try {
+    const client = new MongoClient(process.env.MONGO_URI)
+    client.connect()
+    db = client.db("Starboot")
+    console.log(chalk.greenBright("Mongo atlas is now connected to this project"))
+} catch (error) {
+    console.log(chalk.redBright("Failure to connect to Mongo"), error)
+}
+
 
 export default db;
+
