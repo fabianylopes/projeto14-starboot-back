eecea8bb1f543ab960b8bbcfde036f2b59f41d03
= <f7lipe@me.com>
2022-05-12 11:55:16 -0300

    upd: get products
    Retorna um produto se id do produto é informado. Retorna todos os produtos, caso contrário

--- src/controllers/coffeesController.js
+++ src/controllers/coffeesController.js
@@ -1,17 +1,25 @@
 import db from "../db.js";
+import { ObjectId } from "mongodb";
 
 export async function getCoffees(req, res){
     /*
-    [x] Buscar produtos no banco de dados 
+    [x] Obter id da requisição 
+    [x] Buscar produto se id for não nulo. Buscar produtos, caso contrário 
     [x] Enviar resposta da requisição 
     */
 
+    const {id} = req.query
+
     try {
         const coffeeCollection = await db.collection("products")
+        if (id) {
+            const coffee = await coffeeCollection.findOne({_id: new ObjectId(id)})
+            return res.status(200).send(coffee)
+        }
         const coffees = await coffeeCollection.find({}).toArray()
         res.status(200).send(coffees)
     } catch (error) {
-        console.log("Deu ruim pra inserir o café no banco", error)
+        console.log("Deu ruim pra buscar o café no banco", error)
     }
 }
 
