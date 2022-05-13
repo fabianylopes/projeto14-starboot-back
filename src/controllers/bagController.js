4547bc2bd21c50ff781104fb602ffb44bd3669ea
= <f7lipe@me.com>
2022-05-13 20:45:32 -0300

    feat: create bag

--- src/controllers/bagController.js
+++ src/controllers/bagController.js
@@ -1,5 +1,38 @@
 import { ObjectId } from "mongodb";
 import db from "../db.js";
+import { v4 as uuid } from 'uuid';
+
+export async function createBag(req, res){
+    const token = uuid()
+    const temporaryToken = uuid()
+try {
+    await db.collection("bag").insertOne(
+        {   
+            owner_id: null, 
+            prodtucts: [],
+            temporaryToken
+        }
+    )
+
+    const bag = await db.collection("bag").findOne({temporaryToken})
+    if (bag){
+        const {_id} = bag
+    await db.collection("session").insertOne({
+        bag_id: _id.toString(), 
+        token
+    })
+
+    await db.collection("bag").updateOne({temporaryToken},
+        { $unset: { temporaryToken: "" }}
+    )
+
+    } 
+    res.status(201).send(token)
+} catch (error) {
+    res.sendStatus(500)
+    console.log("deu ruim para criar a sacola", error)
+}
+}
 
 export async function setBag(req, res) {
 
--- src/routes/bagRoute.js
+++ src/routes/bagRoute.js
@@ -1,10 +1,10 @@
-import { setBag } from "../controllers/bagController.js";
+import { createBag, setBag } from "../controllers/bagController.js";
 import { checkAvaliability } from "../middlewares/validateBag.js";
 import { Router } from "express";
 
 const bagRouter = Router()
 
-
+bagRouter.post('/bag', createBag)
 bagRouter.post('/bag',checkAvaliability, setBag)
 
 
