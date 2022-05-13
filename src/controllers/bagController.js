9cd86448a8e157d667781801b718df4a178a2d92
= <f7lipe@me.com>
2022-05-13 18:58:23 -0300

    feat: setBag
    adiciona um produto na sacola se o mesmo possuir estoque e não já tiver sido adicionado

--- src/controllers/bagController.js
+++ src/controllers/bagController.js
@@ -1,10 +1,30 @@
 import { ObjectId } from "mongodb";
 import db from "../db.js";
 
-export async function setBag(req, res){
+export async function setBag(req, res) {
 
     /*
-    [] insere o produto ao banco de dados 
+    [] atualizar o array produtos, com o novo produto inserido
     */
+
+    try {
+
+        await db.collection("bag").updateOne({ _id: new ObjectId("627ec6ed5a98fc60f4f61b9a") },
+            {
+                $addToSet: {
+                    "products":
+                    {
+                        product_id: "aaaaa",
+                        quantity: 2
+                    }
+                }
+            }
+        )
+            return res.sendStatus(200)
+
+    } catch (error) {
+        return res.sendStatus(500)
+        console.log("Deu ruim pra atualizar a sacola", error)
+    }
 
 }
