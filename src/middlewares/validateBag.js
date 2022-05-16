08bad2ebe9279342762a3cb900ff483dd2a2d391
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-05-16 15:38:57 -0300

    refactor: middlewares

--- src/middlewares/validateBag.js
+++ src/middlewares/validateBag.js
@@ -1,64 +1,46 @@
-import db from "../db.js"
 import { ObjectId } from "mongodb";
 
-async function setCustomerID(req, res, next) {
-    /*
-    [x] obter o id do usuário
-    [x] obter a sacola 
-    [x] verificar se costumer_id === undefined 
-    [x] se falso, next
-    [x] se verdadeiro, inserir o customer_id:= customer_id
-    */
+import db from "../db.js"
 
-    const { newCustomer_id, bag_token } = req.body
+async function setCustomerID(req, res, next) {
+    const { newCustomer_id, bag_token } = req.body;
 
     if (newCustomer_id && bag_token){
         try {
-            const session = await db.collection("session").findOne({token: bag_token })
-            const { bag_id, customer_id } = session
+            const session = await db.collection("session").findOne({token: bag_token });
+            const { bag_id, customer_id } = session;
             if (customer_id) next() //já possui customer_id
             else {
                 await db.collection("bag").updateOne({ _id: new ObjectId(bag_id.toString()) },
                     {
                         $set: { customer_id: newCustomer_id }
                     }
-                )
-    
+                )  
                 return res.sendStatus(200)
             }
         } catch (error) {
-            res.sendStatus(500)
+            res.sendStatus(500);
             console.log(error)
-        }
+        };
     }
-
-    next()
-
+    next();
 }
 
 async function checkAvaliability(req, res, next) {
-    /*
-    [x] Verificar se o produto está disponível em estoque 
-    [x] Se sim, next()
-    [x] Se não, retornar código 403 (forbiden)
-    [x] Se product_id e requiredQuantity não forem passados, retornar código 404 (not found)
-    */
-
     const { product_id } = req.query
     const { requiredQuantity } = req.body
 
-    if (!product_id || !requiredQuantity) return res.sendStatus(404)
-
+    if (!product_id || !requiredQuantity) return res.sendStatus(404);
 
     try {
-        const product = await db.collection("products").findOne({ _id: new ObjectId(product_id) })
-        const { quantity } = product
-        if (quantity < requiredQuantity) return res.status(403).send('Estoque insuficiente')
-        next()
+        const product = await db.collection("products").findOne({ _id: new ObjectId(product_id) });
+        const { quantity } = product;
+        if (quantity < requiredQuantity) return res.status(403).send('Estoque insuficiente');
+
+        next();
     } catch (error) {
-        console.log('Deu ruim pra consultar o estoque do produto', error)
+        console.log('Deu ruim pra consultar o estoque do produto', error);
     }
 }
-
 
 export { checkAvaliability, setCustomerID }
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,34 +1,29 @@
 import { ObjectId } from "mongodb";
+import chalk from 'chalk';
+
 import db from "../db.js";
-import chalk from 'chalk'
+
 async function validateToken(req,res, next){
-    
-    /*
-    [x] Obter o token do header 
-    [x] Verificar token
-    [x] Next 
-    */
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
 
-    const { authorization } = req.headers
-    const token = authorization?.replace('Bearer ', '')
-    if(!token) return res.status(409).send("Token não foi enviado")
+    if(!token) return res.status(409).send("Token não foi enviado");
+
     try {
-        const session = await db.collection("session").findOne({token})
-        if (!session) return res.status(404).send('Token não encontrado')
+        const session = await db.collection("session").findOne({token});
+        if (!session) return res.status(404).send('Token não encontrado');
     
-        const bag_id = session.bag_id
-    const bag = await db.collection("bag").findOne( {_id: new ObjectId(bag_id)})
-    if (!bag) return res.status(404).send("Sacola não encontrada")
+        const bag_id = session.bag_id;
+        const bag = await db.collection("bag").findOne( {_id: new ObjectId(bag_id)});
+        if (!bag) return res.status(404).send("Sacola não encontrada");
 
-    res.locals.bag = bag 
-    res.locals.bag_token = token 
-    
-    next()
-
+        res.locals.bag = bag;
+        res.locals.bag_token = token;
+        
+        next();
     } catch (error) {
         console.log(chalk.red(error))
     }
-
 }
 
 export {validateToken}
