6dea97a6ba637d25a0199a26bb802d7e8210bb7f
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-05-16 15:31:50 -0300

    refactor: coffee route

--- src/controllers/coffeesController.js
+++ src/controllers/coffeesController.js
@@ -2,38 +2,24 @@ import db from "../db.js";
 import { ObjectId } from "mongodb";
 
 export async function getCoffees(req, res){
-    /*
-    [x] Obter id da requisição 
-    [x] Buscar produto se id for não nulo. Buscar produtos, caso contrário 
-    [x] Enviar resposta da requisição 
-    */
-
     const {id} = req.query
 
-    //const id = req.params.coffeeId;
-
     try {
-        const coffeeCollection = await db.collection("products")
+        const coffeeCollection = await db.collection("products");
         if (id) {
-            const coffee = await coffeeCollection.findOne({_id: new ObjectId(id)})
-            return res.status(200).send(coffee)
+            const coffee = await coffeeCollection.findOne({_id: new ObjectId(id)});
+            return res.status(200).send(coffee);
         }
-        const coffees = await coffeeCollection.find({}).toArray()
-        res.status(200).send(coffees)
+        const coffees = await coffeeCollection.find({}).toArray();
+        res.status(200).send(coffees);
     } catch (error) {
         console.log("Deu ruim pra buscar o café no banco", error)
     }
 }
 
 export async function setCoffees(req, res){
-
-    /*
-    [] Validar body da requisição 
-    [] Inserir produto no banco de dados 
-    [] Enviar resposta da requisição 
-    */
-
-    const coffee = req.body
+    const coffee = req.body;
+    
     try {
         const coffeeCollection = await db.collection("products")
         coffeeCollection.insertOne(coffee)
