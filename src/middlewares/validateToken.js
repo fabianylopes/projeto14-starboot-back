c723501a014255bb47521d2bfd07b9be0573099d
= <f7lipe@me.com>
2022-05-16 11:05:32 -0300

    upd: checkout

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -12,7 +12,6 @@ async function validateToken(req,res, next){
     const { authorization } = req.headers
     const token = authorization?.replace('Bearer ', '')
     if(!token) return res.status(409).send("Token não foi enviado")
-
     try {
         const session = await db.collection("session").findOne({token})
         if (!session) return res.status(404).send('Token não encontrado')
